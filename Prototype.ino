370a813c4aa393bacff1beded572c21f64a2ed52
apaullo11 <58279316+apaullo11@users.noreply.github.com>
2024-02-15 17:06:32 -0500

    Initial Commit - LCD library include

---
+++ Prototype.ino
@@ -0,0 +1,13 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd = LiquidCrystal(7, 6, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
