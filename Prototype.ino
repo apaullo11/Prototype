5c631a15c5438c9440e11dc4e20dbd8213dd33fc
apaullo11 <58279316+apaullo11@users.noreply.github.com>
2024-02-16 01:33:00 -0500

    Finish extensible LCD write function

--- Prototype.ino
+++ Prototype.ino
@@ -3,17 +3,23 @@
 #define LCDCOLUMNS 16
 #define LCDROWS 2
 
+#define JOYSTICKXPIN A0
+#define JOYSTICKYPIN A1
+#define JOYSTICKCLK 7
+
 LiquidCrystal lcd = LiquidCrystal(7, 6, 5, 4, 3, 2);
 enum LCDAlignment { left, center, right };
 
-struct LCDtext {
-  LCDtext(String text, LCDAlignment align);
-  String text;
-  LCDAlignment align; 
+struct LCDText {
+  LCDText(String t, LCDAlignment a);
+  // String to be printed to the LCD display
+  String text = "";
+  // Preferred alignment of the text on the LCD display (left by default)
+  LCDAlignment align = left; 
 };
 
 enum EmuState { selection, snake, pong, doom };
-enum GameStates { unactivated = 0, activated = 1, running = 2, failure = 4 };
+enum GameStates { unactivated, activated, playing, failure = -1 };
 
 EmuState emu = selection;
 GameStates game1 = unactivated;
@@ -26,33 +32,88 @@ void setup() {
   lcd.noCursor();
   lcd.clear();
 
+  LCDWrite(lcd, LCDText("Left",left), LCDText("Right",right));
+
   // prevent main loop from starting if vars aren't initialized properly
   while (emu != selection || game1 != unactivated || game2 != unactivated || game3 != unactivated);
 }
 
 void loop() {
-  // main game state 
+  // main game states
   switch (emu) {
+    // game selection state
     case (selection):
+
       break;
+
     case (snake):
+      switch (game1)
+        case (unactivated):
+
       break;
+
     case (pong):
       break;
+
     case (doom):
       break;
+
     default:
       break;
   }
 
 }
 
-// Writes input String a to column 1 and String b to column 2
-void DisplayWrite(/*std::intializer_list<LCDtext> args*/) {
-  /*
-  lcd.setCursor(0,0);
-  lcd.print();
-  lcd.setCursor(0,1);
-  lcd.print();
-  */
+/*
+template <typename T>
+int arrSize(T t[]) {
+  return t/sizeOf(T);
+}
+*/
+
+// LCDText object constructor with String and LCDAlignment inputs to set initial vars
+LCDText::LCDText(String t, LCDAlignment a) {
+  text = t;
+  align = a;
+}
+
+// Writes Strings to an input LCD display and adjusts the text positioning based on an input LCDText data structure
+// - Text that is longer than the column number will be cut off (limited to a max of 128 columns)
+// - Any args provided that surpass the row number will not be used (limited to a max of 128 rows)
+void LCDWrite(LiquidCrystal LCD, LCDText t, ...) {
+  // prepare list of all args
+  va_list list;
+  va_start(list, t);
+
+  for (uint8_t i = 0; i < LCDROWS; i++) {
+
+    // set text indent based on input alignment - if length is greater than columns, set to 0
+    uint8_t indent = (LCDCOLUMNS < t.text.length()) ? 0 : (LCDCOLUMNS - t.text.length()) ; 
+    switch (t.align) {
+      // no indent
+      case (left):
+        indent = 0;
+        break;
+
+      // half of column difference to center text
+      case (center):
+        indent = indent/2;
+        break;
+
+      // keep indent as column difference
+      case (right):
+        break;
+
+      default:
+        indent = 0;
+        break;
+    }
+    LCD.setCursor(indent,i);
+    LCD.write(t.text.c_str());
+
+    // to next LCDText object
+    va_arg(list, LCDText);
+  }
+
+  va_end(list);
 }
