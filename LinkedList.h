1add508debba0be8fc0fcf942b32870c611587e2
apaullo11 <58279316+apaullo11@users.noreply.github.com>
2024-02-20 03:00:54 -0500

    Forgot to add data ptr to nodes

--- LinkedList.h
+++ LinkedList.h
@@ -1,10 +1,11 @@
 #include <cstdint>
 
+template <typename T>
 class LinkedList {
   LinkedList();
   LinkedList(unsigned int size);
   unsigned int size = 0;
-  Node *head, *tail;
+  Node<T> *head, *tail;
   
   public:
   void removeNode(unsigned int index);
@@ -16,18 +17,22 @@ class LinkedList {
   void destroyList(uint8_t dir);
 };
 
+template <typename T>
 struct Node {
   Node();
   Node(Node *next, Node *prev);
   Node *next, *prev;
+  T *data;
 };
 
-LinkedList::LinkedList() {
+template <typename T>
+LinkedList<T>::LinkedList() {
   this->head = nullptr;
   this->tail = nullptr;
 }
 
-LinkedList::LinkedList(unsigned int size) {
+template <typename T>
+LinkedList<T>::LinkedList(unsigned int size) {
   Node *tempTail = nullptr; 
   for (unsigned int i = 0; i<size; i++) {
     if (tempTail == nullptr) {
@@ -66,17 +71,20 @@ void LinkedList::addNode(unsigned int index) {
 }
 */
 
-void LinkedList::addFrontNode() {
+template <typename T>
+void LinkedList<T>::addFrontNode() {
   this->head->next = new Node(nullptr, this->head);
   this->head = this->head->next;
 }
 
-void LinkedList::addBackNode() {
+template <typename T>
+void LinkedList<T>::addBackNode() {
   this->tail->prev = new Node(this->tail, nullptr);
   this->tail = this->tail->prev;
 }
 
-void LinkedList::removeFrontNode() {
+template <typename T>
+void LinkedList<T>::removeFrontNode() {
   Node *oldHead = this->head;
   // Set new head
   this->head = oldHead->prev;
@@ -86,7 +94,8 @@ void LinkedList::removeFrontNode() {
   delete oldHead;
 }
 
-void LinkedList::removeBackNode() {
+template <typename T>
+void LinkedList<T>::removeBackNode() {
   Node *oldTail = this->tail;
   // Set new tail
   this->tail = oldTail->next;
@@ -96,7 +105,8 @@ void LinkedList::removeBackNode() {
   delete oldTail;
 }
 
-void LinkedList::destroyList(uint8_t dir) {
+template <typename T>
+void LinkedList<T>::destroyList(uint8_t dir) {
   Node *n;
   if (dir<0) {
     n = this->tail;
@@ -123,12 +133,14 @@ void LinkedList::destroyList(uint8_t dir) {
   }
 }
 
-Node::Node() {
+template <typename T>
+Node<T>::Node() {
   this->next = nullptr;
   this->prev = nullptr;
 }
 
-Node::Node(Node *next, Node *prev) {
+template <typename T>
+Node<T>::Node(Node *next, Node *prev) {
   this->next = next;
   this->prev = prev;
 }
